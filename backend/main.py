81c4c29bc5f3ad64e36e060acb92b290f0da7554
Harrison <harrison.pinto@d4d.com.co>
2023-06-07 14:29:57 -0500

    Add CORS middleware to backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-
 from src.prisma import prisma
 
 app = FastAPI()
+
+# Replace with our own frontend URL
+origins = [
+    "http://localhost:3000",
+]
+
+# Add middleware to set CORS headers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
