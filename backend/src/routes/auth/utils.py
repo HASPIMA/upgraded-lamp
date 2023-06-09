564a0c5ba0c6cf0152e0017155ac75d86a406516
Harrison <harrison.pinto@d4d.com.co>
2023-06-09 16:04:10 -0500

    Implement password verification

--- backend/src/routes/auth/utils.py
+++ backend/src/routes/auth/utils.py
@@ -1,6 +1,7 @@
 import hashlib
-from base64 import b64encode
+from base64 import b64encode, b64decode
 from typing import Optional
+from prisma.models import usuarios
 from os import urandom
 
 
@@ -33,3 +34,25 @@ def hash_password(password: str, salt: Optional[bytes] = None) -> tuple[str, str
     )
 
     return b64encode(hashed_password).decode('utf-8'), b64encode(salt).decode('utf-8')
+
+
+def verify_password(user: usuarios, password: str) -> bool:
+    """
+    Verifies a password against the one stored in the database.
+
+    Parameters
+    ----------
+    user : prisma.models.usuarios
+        The user to verify the password against.
+    password : str
+        The password to verify.
+
+    Returns
+    -------
+    bool
+        Whether the password is correct or not.
+    """
+
+    hashed_password, _ = hash_password(password, b64decode(user.salt))
+
+    return user.contrasena == hashed_password
