0d89f120b7561da2431b995986d53f87d2401a6e
Harrison <harrison.pinto@d4d.com.co>
2023-06-09 15:49:16 -0500

    Add constraints to user model

--- backend/src/types/user.py
+++ backend/src/types/user.py
@@ -1,8 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    constr,
+)
 
 
 class SignupUser(BaseModel):
-    nombre: str
-    identificacion: str
+    nombre: constr(strip_whitespace=True, min_length=1)  # type: ignore
+    identificacion: constr(strip_whitespace=True, min_length=1)  # type: ignore
     correo_electronico: EmailStr
-    contrasena: str
+    contrasena: constr(min_length=8)  # type: ignore
