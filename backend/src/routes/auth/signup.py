460ad3b8fb10a4d235c8d76eed73afc046e2a160
Harrison <harrison.pinto@d4d.com.co>
2023-06-09 15:27:06 -0500

    Create user on signup

--- backend/src/routes/auth/signup.py
+++ backend/src/routes/auth/signup.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, Response
+from prisma.client import get_client
+from prisma.errors import PrismaError
 from src.types.user import SignupUser
+
+from .utils import hash_password
 
 router = APIRouter(prefix="/signup")
 
@@ -9,7 +13,31 @@ async def create_user(body: SignupUser, response: Response):
     data = None
     errors = []
 
-    return {
-        "data": data,
-        "errors": errors,
-    }
+    try:
+        # hash the password
+        hashed_password, salt = hash_password(body.contrasena)
+
+        # create the user
+        data = await get_client().usuarios.create(
+            data={
+                'nombre': body.nombre,
+                'identificacion': body.identificacion,
+                'correo_electronico': body.correo_electronico,
+                'contrasena': hashed_password,
+                'salt': salt,
+            }
+        )
+
+        # remove the hashed password and salt from the response
+        data.contrasena = ''
+        data.salt = ''
+    except PrismaError as e:
+        errors.append(e)
+
+        response.status_code = 500
+
+    finally:
+        return {
+            "data": data,
+            "errors": errors,
+        }
