ddab27554c3d4b1f7a0d4e32161742bd6aa47fff
Harrison <harrison.pinto@d4d.com.co>
2023-06-09 13:14:32 -0500

    Add typings and docstring to `hash_password`
    Hashed passwords and salts are returned as base64 encoded strings. This
    commit adds typings and a docstring to the `hash_password` function to
    reflect this.

--- backend/src/routes/auth/utils.py
+++ backend/src/routes/auth/utils.py
@@ -1,9 +1,29 @@
 import hashlib
+from base64 import b64encode
+from typing import Optional
 from os import urandom
 
 
-def hash_password(password: str):
-    salt = urandom(32)
+def hash_password(password: str, salt: Optional[bytes] = None) -> tuple[str, str]:
+    """
+    Hashes a password using the scrypt algorithm.
+
+    Parameters
+    ----------
+    password : str
+        The password to hash.
+    salt : Optional[bytes], optional
+        The salt to use. If not provided, a random salt will be generated.
+
+    Returns
+    -------
+    tuple[str, str]
+        A tuple containing the hashed password and the salt used.
+    """
+
+    if salt is None:
+        salt = urandom(32)
+
     hashed_password = hashlib.scrypt(
         password.encode('utf-8'),
         salt=salt,
@@ -12,4 +32,4 @@ def hash_password(password: str):
         p=1,
     )
 
-    return hashed_password, salt
+    return b64encode(hashed_password).decode('utf-8'), b64encode(salt).decode('utf-8')
