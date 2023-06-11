ecc37bd45a84a516c0dc79240e118281d6fa375e
Harrison <harrison.pinto@d4d.com.co>
2023-06-10 20:42:50 -0500

    Expose login route

--- backend/src/routes/auth/__init__.py
+++ backend/src/routes/auth/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .signup import router as signup_router
+from .login import router as login_router
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 router.include_router(signup_router)
+router.include_router(login_router)
