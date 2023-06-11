6677e2705c29bc5117993ba6803218612d72bc02
Harrison <harrison.pinto@d4d.com.co>
2023-06-11 10:59:01 -0500

    Check if the token's subject and user id are valid

--- backend/src/auth/JWTBearer.py
+++ backend/src/auth/JWTBearer.py
@@ -53,6 +53,25 @@ class JWTBearer(HTTPBearer):
                     detail=response,
                 )
 
+            uid: Optional[int] = decoded_token.get('sub')
+            user_token: Optional[dict] = decoded_token.get('user')
+
+            # TODO: Check if the user exists in the database
+            # TODO: Check subject and user id match
+
+            # Prepare the response
+            add_error('Invalid token.')
+
+            # Check if the token's subject and user id are valid
+            if uid is None or \
+                    user_token is None or \
+                    not isinstance(uid, int) or \
+                    not isinstance(user_token, dict):
+                raise HTTPException(
+                    status_code=400,
+                    detail=response,
+                )
+
             return decoded_token
 
         add_error('Invalid authorization code.')
