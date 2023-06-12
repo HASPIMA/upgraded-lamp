8d003f8f6736e7701c31272ca489a1ac112ce397
Harrison <harrison.pinto@d4d.com.co>
2023-06-11 21:24:12 -0500

    Expose logout endpoint

--- backend/src/routes/auth/__init__.py
+++ backend/src/routes/auth/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .signup import router as signup_router
 from .login import router as login_router
+from .logout import router as logout_router
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 router.include_router(signup_router)
 router.include_router(login_router)
+router.include_router(logout_router)
