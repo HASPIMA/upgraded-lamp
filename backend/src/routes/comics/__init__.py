216b3de40e154e51cf79cfc2b10195e75119cafc
Harrison <harrison.pinto@d4d.com.co>
2023-06-12 15:34:18 -0500

    Expose favorites comics endpoints

--- backend/src/routes/comics/__init__.py
+++ backend/src/routes/comics/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .root import router as root_router
+from .favorites import router as favorites_router
 
 router = APIRouter(prefix="/comics", tags=["comics"])
 
 router.include_router(root_router)
+router.include_router(favorites_router)
