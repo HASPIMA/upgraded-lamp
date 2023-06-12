a8de488c8c725cf4f7e9f9e5226989e78cb74008
Harrison <harrison.pinto@d4d.com.co>
2023-06-11 22:35:10 -0500

    Generate params for Marvel API requests

--- backend/src/routes/comics/utils.py
+++ backend/src/routes/comics/utils.py
@@ -2,6 +2,8 @@ from datetime import datetime, timezone
 from hashlib import md5
 from os import getenv
 
+from src.types.marvel import MarvelParameters
+
 
 MARVEL_PRIVATE_KEY = getenv('MARVEL_PRIVATE_KEY', '')
 MARVEL_PUBLIC_KEY = getenv('MARVEL_PUBLIC_KEY', '')
@@ -23,3 +25,21 @@ def generate_hash() -> tuple[str, float]:
 
     hash = md5(data).hexdigest()
     return hash, timestamp
+
+
+def generate_params() -> MarvelParameters:
+    """
+    Generates the params to be used in the Marvel API requests.
+
+    Returns
+    -------
+    src.types.marvel.MarvelParameters
+        A dictionary containing the params.
+    """
+    hash, timestamp = generate_hash()
+
+    return {
+        "apikey": MARVEL_PUBLIC_KEY,
+        "ts": timestamp,
+        "hash": hash,
+    }
