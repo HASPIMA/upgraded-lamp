5426b82b018c567fee9c4c98c1ee375c7f62eeaf
Harrison <harrison.pinto@d4d.com.co>
2023-06-12 13:41:46 -0500

    Create get comic by id endpoint

--- backend/src/routes/comics/root.py
+++ backend/src/routes/comics/root.py
@@ -3,6 +3,8 @@ from typing import Annotated, Optional
 import httpx
 from fastapi import APIRouter, Query, Response, status
 from src.types.dependencies import AuthenticationDependant
+from src.types.marvel import Comic
+from src.types.responses import ComicResponse
 
 from .utils import MARVEL_COMICS_URL, generate_params
 
@@ -66,3 +68,39 @@ async def get_comics(
             'data': data,
             'errors': errors
         }
+
+
+@router.get("/{comic_id}")
+async def get_comic_by_id(
+    _: AuthenticationDependant,
+    response: Response,
+    comic_id: int,
+) -> ComicResponse:
+    response_endpoint = ComicResponse()
+
+    try:
+        async with httpx.AsyncClient() as client:
+            params = generate_params()
+
+            response_marvel = await client.get(
+                f"{MARVEL_COMICS_URL}/{comic_id}",
+                params=params,  # type: ignore
+            )
+
+        comic = response_marvel.json()
+        comic = comic['data']['results'][0]
+
+        response_endpoint.data = Comic(
+            id=comic['id'],
+            title=comic['title'],
+            description=comic['description'],
+            image=f"{comic['thumbnail']['path']}.{comic['thumbnail']['extension']}"
+        )
+
+    except Exception as e:
+        response_endpoint.errors.append(str(e))
+
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    finally:
+        return response_endpoint
